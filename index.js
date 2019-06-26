a87e6eeb41dc116dba0b740d3ddb03a298eae677
ist188123 <paulorosario@sapo.pt>
2019-06-26 17:29:18 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const http = require('http');
+
+
+client.on("message", async (msg) => {
+//console.log(tamanhoFicheiro)
+var mensagem=msg.content;
+
+
+if (mensagem.startsWith('!ola')) {
+
+ msg.channel.send({
+                    embed: {
+                        color: 3447003,
+                        description: "Grupo Pinhal novo\nHarry Potter Wizard Unit\n\"
+                    }
+                });
+
+
+}
+
+
+
+
+
+});
+
+client.login(process.env.BOT_TOKEN);
